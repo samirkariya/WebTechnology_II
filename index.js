7b733f31c230209ceba592c0f4c78e52ca08ef15
samirkariya <samir.kariya@darshan.ac.in>
2026-04-07 20:35:04 +0530

    Unit 5/API/index.js

--- index.js
+++ index.js
@@ -1,40 +1,24 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const Product = require('./models/product');
+const express = require("express")
+const cors = require("cors")
+const apiKeyAuth = require("./middleware")
 
-const app = express();
+const app = express()
+app.use(cors())
 app.use(express.json())
-app.use(cors());
-app.use(express.static('public'));
 
-mongoose.connect('mongodb://localhost:27017/API_D2D')
-.then(()=>console.log('Database Connected!'))
-.catch(err=>console.log(err));
-
-app.post('/products/add',async(req,res)=>{
-    const data = new Product(req.body);
-    await data.save();
-    res.send('Product added.');
+app.get("/", (req,res)=>{
+ res.send("Public API - No API Key Required")
 })
 
-app.get('/products',async(req,res)=>{
-    const data = await Product.find();
-    res.json(data);
+app.get("/products", apiKeyAuth , (req,res)=>{
+ res.json([
+  {id:1,name:"Laptop"},
+  {id:2,name:"Mobile"},
+  {id:3,name:"Keyboard"}
+ ])
+
 })
 
-app.get('/products/:id',async(req,res)=>{
-    const data = await Product.findOne({productId:req.params.id});
-    res.json(data);
+app.listen(5000,()=>{
+ console.log("Server running on port 5000")
 })
-
-app.delete('/products/:id',async(req,res)=>{
-    const data = await Product.findOneAndDelete({productId:req.params.id});
-    res.send('Product deleted');
-});
-
-app.put('/products/:id',async(req,res)=>{
-    const data = await Product.findOneAndUpdate({productId:req.params.id}, req.body);
-    res.json(data);
-});
-app.listen(3000,()=>console.log('Server is running on 3000'));
